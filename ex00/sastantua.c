1b5d1c96a3986e195ffc333a9a1dd97df0cc43cd
Damien Abeloos <dabeloos@w-r1-p1.s19.be>
2018-08-12 00:37:56 +0200

    finished

--- ex00/sastantua.c
+++ ex00/sastantua.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/11 12:29:57 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/11 17:27:36 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/12 00:37:19 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,44 @@ int		ft_putchar(char c)
 	return (0);
 }
 
+int		ft_ignore_space(const char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] == ' ')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int		ft_atoi(const char *str)
+{
+	int		result;
+	int		sign;
+	int		i;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	while (str[i] == ' ')
+		i++;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		i++;
+		sign = -1;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * result);
+}
+
 void	sastantua(int size)
 {
 	int		floor;
@@ -26,15 +64,28 @@ void	sastantua(int size)
 	int		height;
 	int		l;
 	int		c;
+	int		i_floor;
+	int		floor_height;
+	int		incremental_length;
+	int		door_size;
 
 	if (size > 0)
 	{
+		door_size = size;
+		if (door_size % 2 == 0)
+			door_size -= 1;
 		base = 3;
+		floor_height = 3;
+		i_floor = floor_height - 1;
 		height = base * size;
 		floor = 0;
+		incremental_length = 0;
 		while (++floor < size)
+		{
 			height += floor;
-		length = base + height * 2 + (size - 1) * 6;
+			incremental_length += ((floor - 1) / 2) * 2;
+		}
+		length = base + (height - 1) * 2 + (size - 1) * 4 + incremental_length;
 		l = -1;
 		floor = 0;
 		while (++l < height)
@@ -42,11 +93,36 @@ void	sastantua(int size)
 			c = -1;
 			while (++c < length)
 			{
-				if (c < (length - base) / 2 || c >= (length - base) / 2 + base)
+				if (c < (length - base) / 2)
 					ft_putchar(' ');
+				else if (c >= (length - base) / 2 + base)
+					break ;
+				else if (c == (length - base) / 2)
+					ft_putchar('/');
+				else if (c == (length - base) / 2 + base - 1)
+					ft_putchar('\\');
+				else if (l == height - door_size / 2 - 1 &&
+						c == length / 2 + door_size / 2 - 1 &&
+						door_size / 2 > 0 &&
+						c != length / 2)
+					ft_putchar('$');
+				else if (c >= length / 2 - door_size / 2 && 
+						c <= length / 2 + door_size / 2 &&
+						l >= height - door_size &&
+						l <= height)
+					ft_putchar('|');
 				else
 					ft_putchar('*');
 			}
+			if (i_floor == 0)
+			{
+				floor++;
+				floor_height = floor_height + 1;
+				i_floor = floor_height - 1;
+				base += 4 + ((floor - 1) / 2) * 2;
+			}
+			else
+				i_floor--;
 			base += 2;
 			ft_putchar('\n');
 		}
@@ -57,7 +133,10 @@ int		main(int argc, char *argv[])
 {
 	int i;
 
-	i = *argv[1] - '0';
-	sastantua(i);
+	if (argc == 2)
+	{
+		i = ft_atoi(argv[1]);
+		sastantua(i);
+	}
 	return (0);
 }
