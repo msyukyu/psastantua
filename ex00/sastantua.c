fcbe73d3e5973c3cc10f9dcb598ed29aaae1bc2a
Damien Abeloos <dabeloos@w-r1-p1.s19.be>
2018-08-12 02:55:42 +0200

    finished

--- ex00/sastantua.c
+++ ex00/sastantua.c
@@ -6,17 +6,11 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/11 12:29:57 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/12 01:34:13 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/12 02:53:15 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-int		ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (0);
-}
+int		ft_putchar(char c);
 
 int		ft_atoi(const char *str)
 {
@@ -44,74 +38,89 @@ int		ft_atoi(const char *str)
 	return (sign * result);
 }
 
+void	ft_define_var(int size, int *var)
+{
+	var[7] = size;
+	if (var[7] % 2 == 0)
+		var[7] -= 1;
+	var[5] = 3;
+	var[4] = 3;
+	var[3] = var[4] - 1;
+	var[1] = var[5] * size;
+	var[2] = 0;
+	var[6] = 0;
+	while (++var[2] < size)
+	{
+		var[1] += var[2];
+		var[6] += ((var[2] - 1) / 2) * 2;
+	}
+	var[0] = var[5] + (var[1] - 1) * 2 + (size - 1) * 4 + var[6];
+	var[2] = 0;
+}
+
+void	ft_write_line(int c, int l, int *var)
+{
+	while (++c < var[0])
+	{
+		if (c < (var[0] - var[5]) / 2)
+			ft_putchar(' ');
+		else if (c >= (var[0] - var[5]) / 2 + var[5])
+			break ;
+		else if (c == (var[0] - var[5]) / 2)
+			ft_putchar('/');
+		else if (c == (var[0] - var[5]) / 2 + var[5] - 1)
+			ft_putchar('\\');
+		else if (l == var[1] - var[7] / 2 - 1 &&
+				c == var[0] / 2 + var[7] / 2 - 1 &&
+				var[7] / 2 > 0 &&
+				c != var[0] / 2)
+			ft_putchar('$');
+		else if (c >= var[0] / 2 - var[7] / 2 &&
+				c <= var[0] / 2 + var[7] / 2 &&
+				l >= var[1] - var[7] &&
+				l <= var[1])
+			ft_putchar('|');
+		else
+			ft_putchar('*');
+	}
+}
+
+/*
+** index definition for var[]
+** var[0] : length of the longest line (count in characters)
+** var[1] : height of the pyramid (count in lines)
+** var[2] : index of the current floor
+** var[3] : index of the line inside the current floor
+** var[4] : height of the current floor (count in lines)
+** var[5] : length of the current line (count in characters that are not ' ')
+** var[6] : count incremental characters (count in characters)
+** var[7] : size of the door (count in characters == in lines)
+*/
+
 void	sastantua(int size)
 {
-	int		floor;
-	int		base;
-	int		length;
-	int		height;
 	int		l;
 	int		c;
-	int		i_floor;
-	int		floor_height;
-	int		incremental_length;
-	int		door_size;
+	int		var[8];
 
 	if (size > 0)
 	{
-		door_size = size;
-		if (door_size % 2 == 0)
-			door_size -= 1;
-		base = 3;
-		floor_height = 3;
-		i_floor = floor_height - 1;
-		height = base * size;
-		floor = 0;
-		incremental_length = 0;
-		while (++floor < size)
-		{
-			height += floor;
-			incremental_length += ((floor - 1) / 2) * 2;
-		}
-		length = base + (height - 1) * 2 + (size - 1) * 4 + incremental_length;
+		ft_define_var(size, var);
 		l = -1;
-		floor = 0;
-		while (++l < height)
+		while (++l < var[1])
 		{
 			c = -1;
-			while (++c < length)
+			ft_write_line(c, l, var);
+			if (var[3] == 0)
 			{
-				if (c < (length - base) / 2)
-					ft_putchar(' ');
-				else if (c >= (length - base) / 2 + base)
-					break ;
-				else if (c == (length - base) / 2)
-					ft_putchar('/');
-				else if (c == (length - base) / 2 + base - 1)
-					ft_putchar('\\');
-				else if (l == height - door_size / 2 - 1 &&
-						c == length / 2 + door_size / 2 - 1 &&
-						door_size / 2 > 0 &&
-						c != length / 2)
-					ft_putchar('$');
-				else if (c >= length / 2 - door_size / 2 &&
-						c <= length / 2 + door_size / 2 &&
-						l >= height - door_size &&
-						l <= height)
-					ft_putchar('|');
-				else
-					ft_putchar('*');
-			}
-			if (i_floor == 0)
-			{
-				floor++;
-				floor_height = floor_height + 1;
-				i_floor = floor_height - 1;
-				base += 4 + ((floor - 1) / 2) * 2;
+				var[2]++;
+				var[4] = var[4] + 1;
+				var[3] = var[4] - 1;
+				var[5] += 4 + ((var[2] - 1) / 2) * 2;
 			}
 			else
-				i_floor--;
-			base += 2;
+				var[3]--;
+			var[5] += 2;
 			ft_putchar('\n');
 		}
 	}
